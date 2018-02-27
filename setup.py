7a842cc7f6b60059f31ef8683cd2949c76f399a3
Stefan Schweer <s.schweer@douglas.de>
2018-02-28 00:53:52 +0100

    change license to MIT

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     author_email='s.schweer@douglas.de',
 
     # Choose your license
-    license='WTFPL',
+    license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -50,11 +50,13 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: WTFPL License',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
     ],
 
     # What does your project relate to?
